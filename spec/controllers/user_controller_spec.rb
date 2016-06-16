162370f3016ce9862a560b387ffbb5331ab94fb4
Emily <ebosakowski@gmail.com>
2016-06-16 14:48:58 -0500

    Write user registration tests

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-describe UserController do
+describe UsersController do
 
+  describe 'POST #create', type: :controller do
+
+    it "Adds a new user to the database" do
+      expect{ post :create, { user: {username: "spanky", password: "password" } } }.to change(User, :count).by(1)
+    end
+
+    it "redirects newly registered user to their page" do
+      post :create, { user: {username: "spanky", password: "password" } }
+        expect(response.status).to eq(302)
+    end
+
+    it "redirects newly registered user to their page" do
+      post :create, { user: {username: "spanky", password: "password" } }
+      expect(session[:user_id]).to_not eq(nil)
+    end
+
+  end
 end
